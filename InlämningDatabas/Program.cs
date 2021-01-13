4b8c93d2d168f65bfc6c1d34a7d2065a15cd48da
Thera1337 <70898199+Thera1337@users.noreply.github.com>
2021-01-13 10:39:39 +0100

    Add project files.

---
+++ InlämningDatabas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InlämningDatabas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
